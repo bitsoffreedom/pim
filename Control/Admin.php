3a1f9988ddb74a34c29ec4ff4acd4d836a3eca21
Alexander Schrijver <alexander.schrijver@gmail.com>
2010-08-23 17:17:47 +0200

    Update style in the admin controller too.

--- Control/Admin.php
+++ Control/Admin.php
@@ -3,45 +3,46 @@ require_once( PIM_BASE_PATH . '/Control/Controller.php' );
 
 class Control_Admin extends Control_Controller
 {
+	/**
+	* Constructor
+	* @param Route $r
+	*/
+	public function __construct( Route $r )
+	{
+		parent::__construct( $r );
+	}
 
+	public function execGet()
+	{
+		// First see if we're already logged in
+		if ( $this->sessionOK() ) {
+		    
+		}
+		else {
 
-    /**
-     * Constructor
-     * @param Route $r
-     */
-    public function __construct( Route $r ) {
-        parent::__construct( $r );
-    }
+		}
+	}
 
-    public function execGet() {
-        // First see if we're already logged in
-        if ( $this->sessionOK() ) {
-            
-        }
-        else {
+	public function execPost()
+	{
+		// First see if we're already logged in
+		if ( $this->sessionOK() ) {
 
-        }
-    }
+		}
+		else {
+		    
+		}
+	}
 
-    public function execPost() {
-        // First see if we're already logged in
-        if ( $this->sessionOK() ) {
+	/**
+	*
+	* @return bool
+	*/
+	private function sessionOK() {
+		$session = Session::get();
+		$session->start();
 
-        }
-        else {
-            
-        }
-    }
-
-    /**
-     *
-     * @return bool
-     */
-    private function sessionOK() {
-        $session = Session::get();
-        $session->start();
-
-        return ( isset( $session->user )
-                && $session->user instanceof Session_User );
-    }
+		return ( isset( $session->user )
+		    && $session->user instanceof Session_User );
+	}
 }
