3adc8e240873385760685f55d9f45b4692a51065
Alexander Schrijver <alexander.schrijver@gmail.com>
2010-12-02 13:50:08 +0100

    Rename Role/citizensconsumerrole to ConsumerRelation Make HasMy* a many-to-many relation Add fixtures Update template layout

--- letter/models.py
+++ letter/models.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.db import models
 
 class City(models.Model):
@@ -24,7 +26,7 @@ class Sector(models.Model):
 	def __unicode__(self):
 		return self.sector;
 	
-class Role(models.Model):
+class ConsumerRelation(models.Model):
 	role = models.CharField(max_length=20)
 
 	def __unicode__(self):
@@ -40,15 +42,27 @@ class Brand(models.Model):
 	name = models.CharField(max_length=20)
 
 class Identifier(models.Model):
+	""" Identifier which company require to identify a user in the letter """
 	name = models.CharField(max_length=20)
+	identifiers = models.ForeignKey("Organisation")
+
+class HasMy(models.Model):
+	""" Information which companies have about an user"""
+	name = models.CharField(max_length=200, blank=True, help_text="")
+
+	def __unicode__(self):
+		return self.name;
 
 class Organisation(models.Model):
-	preflabel = models.CharField(max_length=200, help_text="De officiele naam van de organisatie")
-	altlabel = models.CharField(max_length=200, blank=True, help_text="Een alternatieve naam voor de organisatie")
+	""" A model representing an organisation. """
+
+	name = models.CharField(max_length=200, verbose_name=_('organisation name'), help_text="De officiele naam van de organisatie")
+	""" Official name of organisation. """
+	shortname = models.CharField(max_length=200, blank=True, help_text="Een korte naam voor een organisatie")
 	kvknumber = models.CharField(max_length=200, blank=True)
-	street_address = models.CharField(max_length=200, blank=True)
-	housenr = models.CharField(max_length=20, blank=True)
-	answernr = models.CharField(max_length=20, blank=True)
+	street_address = models.CharField(max_length=200, verbose_name=_('street address'), blank=True)
+	housenr = models.CharField(max_length=20, verbose_name=_('house number'), blank=True)
+	answernr = models.CharField(max_length=20, verbose_name=_('answer number'), blank=True)
 	postbus = models.CharField(max_length=20, blank=True)
 	postcode = models.CharField(max_length=20, blank=True)
 	city = models.ForeignKey(City, blank=True, null=True)
@@ -57,24 +71,21 @@ class Organisation(models.Model):
 	website = models.CharField(max_length=200, blank=True)
 	brands = models.ManyToManyField(Brand, blank=True, null=True)
 	keyword = models.ManyToManyField(Keyword, blank=True, null=True)
-	hasmyname = models.BooleanField()
-	hasmystreetaddress = models.BooleanField()
-	hasmytelephonenumber = models.BooleanField()
-	hasmyairmilecardied = models.BooleanField()
-	hasmycardlicensenumber = models.BooleanField()
 	notificationnumber = models.DecimalField(max_digits=10,
 		decimal_places=0, blank=True, null=True, help_text="CBP registernummer")
-	citizenorconsumerrole = models.ManyToManyField(Role, blank=True,
+	consumerrelation = models.ManyToManyField(ConsumerRelation, blank=True,
 		null=True, help_text="Rol van de burger/consument ten opzichte van de" \
 		"maatschappij en dienstverleners en andere organisaties")
 	relation = models.ManyToManyField("self", through="Relation",
 		symmetrical=False, blank=True, null=True)
-	identifiers = models.ManyToManyField(Identifier, blank=True, null=True)
+	hasmy = models.ManyToManyField(HasMy)
 
 	def __unicode__(self):
-		return self.preflabel;
+		return self.name;
 
 class Relation(models.Model):
+	""" A relation between two companies """
 	src = models.ForeignKey(Organisation, related_name='src')
 	dest = models.ForeignKey(Organisation, related_name='dest')
 	confidence = models.DecimalField(max_digits=10, decimal_places=0)
+
