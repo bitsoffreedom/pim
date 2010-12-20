a670bf01bb666d2cec479d8b6faf943e4588b76f
Alexander Schrijver <alexander.schrijver@gmail.com>
2010-12-20 09:44:51 +0100

    Oops, accidentally kept runserver.sh in my staging area.

--- runserver.sh
+++ runserver.sh
@@ -7,22 +7,20 @@ BASEPATH=`basename $PWD`
 PYTHON=python
 SCREEN=screen
 ENVDIR=env
+HOSTNAME=`hostname`
+PORTNUMBER=8000
 
 if [ -d $ENVDIR ]; then
     echo 'Using python from virtualenv environment' >&2
     PYTHON=$ENVDIR/bin/python
 fi
 
-if [ ! -f portnumber ]; then
-    echo 'No port number set!' >&2
-    echo 'Please create a file named "portnumber" in the current directory with' >&2
-    echo 'a locally unique portnumber in it, like "1234".' >&2
-    exit 1
-fi
+[ -f hostname ] && HOSTNAME=`cat hostname`
+[ -f portnumber ] && PORTNUMBER=`cat portnumber`
 
 if [ $1 ]; then
     $PYTHON manage.py $*
 else
-    $SCREEN -S $BASEPATH $PYTHON manage.py runserver `hostname`:`cat portnumber`
+    $SCREEN -S $BASEPATH $PYTHON manage.py runserver $HOSTNAME:$PORTNUMBER
 fi
 
