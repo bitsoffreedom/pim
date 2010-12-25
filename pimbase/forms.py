3dfcc8e7c72fb9defcf1235c261cc30128b5d357
Alexander Schrijver <alexander.schrijver@gmail.com>
2010-12-25 17:11:29 +0100

    Convert from tabs to 4 spaces. This seems to be the django (python?) preferred form.

--- pimbase/forms.py
+++ pimbase/forms.py
@@ -2,18 +2,18 @@ from django import forms
 from models import Identifier
 
 class UserForm(forms.Form):
-	def __init__(self, request, *args, **kwargs):
-		super(UserForm, self).__init__(*args, **kwargs)
+    def __init__(self, request, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
 
-		request.session.setdefault('companies', [])
+        request.session.setdefault('companies', [])
 
-		ids = Identifier.objects.filter(organisation__in = request.session['companies'])
-		if ids:
-			for i in ids:
-				self.fields['misc_%d' % (i.pk, )] = forms.CharField(label = '%s %s' % (i.organisation.short_name, i.name))
+        ids = Identifier.objects.filter(organisation__in = request.session['companies'])
+        if ids:
+            for i in ids:
+                self.fields['misc_%d' % (i.pk, )] = forms.CharField(label = '%s %s' % (i.organisation.short_name, i.name))
 
-	firstname = forms.CharField(label="Voornaam")
-	lastname = forms.CharField(label="Achternaam")
-	street_address = forms.CharField(label="Adres")
-	postcode = forms.CharField(label="Postcode")
-	city = forms.CharField(label="Stad")
+    firstname = forms.CharField(label="Voornaam")
+    lastname = forms.CharField(label="Achternaam")
+    street_address = forms.CharField(label="Adres")
+    postcode = forms.CharField(label="Postcode")
+    city = forms.CharField(label="Stad")
