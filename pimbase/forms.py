56237a954cc3df67ebcccdaed7d91347ea475583
Alexander Schrijver <alexander.schrijver@gmail.com>
2011-02-22 19:33:35 +0100

    Disable "dynamic fields" which currently aren't used.

--- pimbase/forms.py
+++ pimbase/forms.py
@@ -5,12 +5,12 @@ class UserForm(forms.Form):
     def __init__(self, request, *args, **kwargs):
         super(UserForm, self).__init__(*args, **kwargs)
 
-        request.session.setdefault('companies', [])
-
-        ids = Identifier.objects.filter(organisation__in = request.session['companies'])
-        if ids:
-            for i in ids:
-                self.fields['misc_%d' % (i.pk, )] = forms.CharField(label = '%s %s' % (i.organisation.short_name, i.name))
+#        request.session.setdefault('companies', [])
+#
+#        ids = Identifier.objects.filter(organisation__in = request.session['companies'])
+#        if ids:
+#            for i in ids:
+#                self.fields['misc_%d' % (i.pk, )] = forms.CharField(label = '%s %s' % (i.organisation.short_name, i.name))
 
     firstname = forms.CharField(label="Voornaam")
     lastname = forms.CharField(label="Achternaam")
