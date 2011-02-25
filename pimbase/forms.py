dfccec0cda5ba1ed4fdad4897b87969149d4d00a
Alexander Schrijver <alexander.schrijver@gmail.com>
2011-02-25 23:54:27 +0100

    Add improved field validation for UserForm as requested by Paul Vogel.

--- pimbase/forms.py
+++ pimbase/forms.py
@@ -1,5 +1,18 @@
 from django import forms
 from models import Identifier
+from django.core import exceptions
+from django.utils.translation import ugettext as _
+from django.contrib.localflavor.nl.forms import NLZipCodeField
+
+class NameField(forms.CharField):
+    # XXX: There is no source for the validity of this invalid characters.
+    invalid_characters = ";|[]{}<>*&^%$#@!`~|\/?+=_:"
+
+    def validate(self, *args, **kwargs):
+        super(NameField, self).validate(*args, **kwargs)
+
+        if args[0] in self.invalid_characters:
+            raise exceptions.ValidationError(_("Please don't enter any of the following characters: %(characters)s") % {'characters': self.invalid_characters, } )
 
 class UserForm(forms.Form):
     def __init__(self, request, *args, **kwargs):
@@ -12,8 +25,8 @@ class UserForm(forms.Form):
 #            for i in ids:
 #                self.fields['misc_%d' % (i.pk, )] = forms.CharField(label = '%s %s' % (i.organisation.short_name, i.name))
 
-    firstname = forms.CharField(label="Voornaam")
-    lastname = forms.CharField(label="Achternaam")
-    street_address = forms.CharField(label="Straat")
-    postcode = forms.CharField(label="Postcode")
-    city = forms.CharField(label="Woonplaats")
+    firstname = NameField(label="Voornaam", max_length=200)
+    lastname = NameField(label="Achternaam", max_length=200)
+    street_address = NameField(label="Straat", max_length=200)
+    postcode = NLZipCodeField(label="Postcode")
+    city = NameField(label="Woonplaats", max_length=200)
