03209606187eb4530865ccd7d57b60a8ec004de7
Rejo Zenger <rejo@zenger.nl>
2011-07-12 16:09:01 +0200

    Removed h2 header from front page. Fixed non-working 'start' button on front page.

--- wp-theme/front-page.php
+++ wp-theme/front-page.php
@@ -9,13 +9,12 @@ get_header(); ?>
 	<!-- Content -->
 	<div id="content">
 		<h1>Ontdek wat organisaties over jou weten</h1>
-		<h2>Een inzageverzoek in drie stappen - met PIM</h2>
 		
 		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 			
 			<?php echo the_content(); ?>
 			
-			<a href="" id="next-step" class="step-button">Start PIM</a>
+			<a href="/start/" id="next-step" class="step-button">Start PIM</a>
 			
 		<?php endwhile; endif; ?>
 		
