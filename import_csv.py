50a4067c183d364bf89258a2d8af76d01a6da311
Alexander Schrijver <alexander.schrijver@gmail.com>
2012-03-05 19:43:26 +0100

    Add a tool to import CSV. Currently adapted to parse a dump of schools.

---
+++ import_csv.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+import sys
+import glob
+
+from pprint import pprint
+
+import django
+from django.core.management import setup_environ
+import settings
+from pimbase.models import *
+setup_environ(settings)
+
+import csv
+
+from import_utils import create_organisation, normalise_city
+
+f = open('import.csv', 'r')
+c = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+
+# Used to generate the following...
+#for r in c:
+#    i=0
+#    for b in r:
+#        print "%s=%d" % (b.replace(" ", "").replace("-", ""), i )
+#        i += 1
+#
+#    break
+
+SOORTHO=0
+PROVINCIE=1
+BEVOEGDGEZAGNUMMER=2
+BRINNUMMER=3
+INSTELLINGSNAAM=4
+STRAATNAAM=5
+HUISNUMMERTOEVOEGING=6
+POSTCODE=7
+PLAATSNAAM=8
+GEMEENTENUMMER=9
+GEMEENTENAAM=10
+DENOMINATIE=11
+TELEFOONNUMMER=12
+INTERNETADRES=13
+STRAATNAAMCORRESPONDENTIEADRES=14
+HUISNUMMERTOEVOEGINGCORRESPONDENTIEADRES=15
+POSTCODECORRESPONDENTIEADRES=16
+PLAATSNAAMCORRESPONDENTIEADRES=17
+NODAALGEBIEDCODE=18
+NODAALGEBIEDNAAM=19
+RPAGEBIEDCODE=20
+RPAGEBIEDNAAM=21
+WGRGEBIEDCODE=22
+WGRGEBIEDNAAM=23
+COROPGEBIEDCODE=24
+COROPGEBIEDNAAM=25
+ONDERWIJSGEBIEDCODE=26
+ONDERWIJSGEBIEDNAAM=27
+RMCREGIOCODE=28
+RMCREGIONAAM=29
+
+
+def insert_school(r):
+    context = {}
+    context['name'] = r[INSTELLINGSNAAM]
+    context['address'] = "%s %s" % (r[STRAATNAAMCORRESPONDENTIEADRES], r[HUISNUMMERTOEVOEGINGCORRESPONDENTIEADRES])
+    context['website'] = r[INTERNETADRES]
+    context['postcode'] = r[POSTCODE]
+    context['city'] = normalise_city(r[PLAATSNAAMCORRESPONDENTIEADRES])
+    context['citizenrole'] = 'school'
+
+    create_organisation(**context)
+
+for r in c:
+    if r[SOORTHO] == "hbo":
+        insert_school(r)
---
+++ import_utils.py
@@ -0,0 +1,64 @@
+import os
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+import sys
+import glob
+
+from pprint import pprint
+
+import django
+from django.core.management import setup_environ
+import settings
+from pimbase.models import *
+setup_environ(settings)
+
+CITY_TRANS = {
+"'S-GRAVENHAGE": "Den Haag",
+"'S-HERTOGENBOSCH": "Den Bosch",
+"EDE GLD": 'Ede',
+"HENGELO OV": 'Hengelo',
+"VELP GLD": 'Velp',
+}
+
+def normalise_city(name):
+    if name in CITY_TRANS:
+        return CITY_TRANS[name]
+
+    return name.title()
+
+def create_organisation(**context):
+    o = Organisation(country=None)
+
+    field_list = ['name', 'addressee', 'postcode', 'address', 'website' ]
+
+    for field in field_list:
+        if field in context:
+            setattr(o, field, context[field])
+
+    try:
+        citizenrole = CitizenRole.objects.get(name = context['citizenrole'])
+    except CitizenRole.DoesNotExist:
+        citizenrole = CitizenRole()
+        citizenrole.name = context['citizenrole']
+        citizenrole.save()
+
+    try:
+        city = City.objects.get(name = context['city'])
+    except City.DoesNotExist:
+        print "Creating city: " + context['city']
+        city = City()
+        city.setname(context['city'])
+        city.save()
+
+    try:
+        country = Country.objects.get(name = 'Nederland')
+    except Country.DoesNotExist:
+        assert 0, "Country 'Nederland' should exist in the Database"
+
+    # Save, so a primary key will be generated (needed for m2m relations)
+    o.save()
+
+    o.city = city
+    o.country = country
+    o.citizenrole.add(citizenrole)
+
+    o.save()
